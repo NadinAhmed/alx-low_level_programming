863b5470407692b2f6ecfc57987687c1867751c7
NadinAhmed <nadinahmed316@gmail.com>
2023-02-16 18:21:27 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The start point of the program.
+ *
+ * Return: Ends the program.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
