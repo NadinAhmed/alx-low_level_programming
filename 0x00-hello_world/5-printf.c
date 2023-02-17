f480d6741708b5ab9e8d7c339d05712124c27ace
NadinAhmed <nadinahmed316@gmail.com>
2023-02-16 18:32:27 -0800

    hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The start point of the program.
+ *
+ * Return: Ends the program.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
