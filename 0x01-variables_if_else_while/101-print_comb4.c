6015df01e827694c4b72730d9d76ae42df88a910
NadinAhmed <nadinahmed316@gmail.com>
2023-02-18 11:06:14 -0800

    three digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - The Starting point of the program
+ *
+ * Return: Ends the program with code 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i == 7 && j == 8 && k == 9)
+				{
+					continue;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
