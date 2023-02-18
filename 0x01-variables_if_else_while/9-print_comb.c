00f10345dc2ab8b7934bf3d16131844b6e1bf8cd
NadinAhmed <nadinahmed316@gmail.com>
2023-02-18 10:25:56 -0800

    numbers with ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - The Starting point of the program
+ *
+ * Return: Ends the program with code 0
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
