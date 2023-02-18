3469ba621faa42a2298e9f260da0dba9fcf7233b
NadinAhmed <nadinahmed316@gmail.com>
2023-02-18 09:46:21 -0800

    numders

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - The Starting point of the program
+ *
+ * Return: Ends the program with code 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
