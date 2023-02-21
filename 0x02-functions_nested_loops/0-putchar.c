918e042831cb26a77a6eecf178416ad069441c14
NadinAhmed <nadinahmed316@gmail.com>
2023-02-21 06:49:35 -0800

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The Starting point of the program
+ *
+ * Return: Ends the program with code 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+}
