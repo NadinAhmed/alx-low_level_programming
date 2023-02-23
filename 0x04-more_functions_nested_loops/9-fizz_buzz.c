9defa17a075905b47a63f2784b5de57b1635a6d4
NadinAhmed <nadinahmed316@gmail.com>
2023-02-23 12:20:32 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,30 +7,30 @@
  */
 int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i != 99)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 15 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i == 99)
-		{
-			printf("FizzBuzz");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	return (0);
+        for (i = 1; i < 100; i++)
+        {
+                if (i % 3 == 0 && i != 99)
+                {
+                        printf("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                        printf("Buzz ");
+                }
+                else if (i % 15 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
+                else if (i == 99)
+                {
+                        printf("FizzBuzz");
+                }
+                else
+                {
+                        printf("%d ", i);
+                }
+        }
+        return (0);
 }
