20343b6dbca51f4219e05aa6e89d7b841e69faee
NadinAhmed <nadinahmed316@gmail.com>
2023-02-22 18:13:27 -0800

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs -> check adsolute given number
+ *
+ * @i: The number you want to check
+ *
+ * Return: absolute value of i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	return (i);
+}
