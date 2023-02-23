1763b443d14e72edaf4dc28378c15bf2f9e5d18b
NadinAhmed <nadinahmed316@gmail.com>
2023-02-23 05:23:11 -0800

    print numbers repeted 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers -> print numbers from 0 to 9 repeted 10 times
+ * return always 0
+ */
+void more_numbers(void)
+{
+	int n, i, count;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			n = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				n = count % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
